79de6054cb5d02db9b8e30175ec3c7e4e5fef041
sophy1 <jihye.sophy.won@gmail.com>
2023-03-31 15:13:20 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-// https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb/rules/react.js
-/*eslint jsx-quotes: ["error", "prefer-double"]*/
-
-// react/no-unknown-property, Unknown property 'b' found
-// jsx-a11y/anchor-has-content, Anchors must have content and the content must be accessible by a screen reader
-<a b='c' />
+"\'";
+'\"';
